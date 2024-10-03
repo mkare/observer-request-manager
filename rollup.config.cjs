b4b266864b26efad6d5b411cbf5ed681219225e6
Mustafa Morbel <mustafa.morbel@cimri.com>
2024-10-03 03:36:33 +0300

    add lite-server for serve demo

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,12 +1,14 @@
+// rollup.config.cjs
 const typescript = require("@rollup/plugin-typescript");
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const { terser } = require("rollup-plugin-terser");
+const path = require("path");
 
 module.exports = {
   input: "src/index.ts",
   output: {
-    file: "bundle.js",
+    file: path.resolve(__dirname, "dist", "bundle.js"), // Output to the dist folder
     format: "iife",
     name: "MyModuleName",
     sourcemap: true,
